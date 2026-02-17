14fa5ca98e45c904fef861741e7a72a5249e41bf
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-16 19:57:48 -0800

    Create boilerplate problem setup

---
+++ 2678_number_of_senior_citizens/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def __init__(self):
+        pass
+        
+    def countSeniors(self, details):
+        pass
+
+    def solve(self, inputs):
+        return self.countSeniors(inputs['details'])
---
+++ 2678_number_of_senior_citizens/tests.py
@@ -0,0 +1,43 @@
+from solution import Solution
+
+class Test:
+    TESTS = [{
+        'TITLE': '',
+        'INPUTS': {
+            'details': -1
+        },
+        'OUTPUT': -1,
+    }]
+
+    def __init__(self):
+        self.solution = new Solution()
+
+    def runTests(self):
+        testsRun = 0
+        testsFailed = 0
+        for test in self.TESTS:
+            print(f'Running test {test["TITLE"]}')
+
+            inputs = test['INPUTS']
+            expected = test['OUTPUT']
+
+            testsRun += 1
+            try:
+                output = self.solution.solve(inputs)
+            except Exception as e:
+                print(f'Error occurred while solving: {e}')
+                testFailed += 1
+                continue
+
+            try:
+                assert(output == expected)
+            except AssertionError:
+                print(f'Test Failed with\nINPUT {inputs}\nEXPECTED {expected}\nACTUAL {output}')
+                testsFailed += 1
+        
+        print(f'Tests Run: {testsRun}')
+        print(f'Tests Succeeded: {testsRun - testsFailed}')
+        print(f'Tests Failed: {testsFailed}')
+
+Tester = new Test()
+Tester.runTests()
