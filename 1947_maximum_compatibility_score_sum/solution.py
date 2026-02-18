e58632f95496d86fd1dda0009f3ca1524b6163d8
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-18 11:10:33 -0800

    Helper function to calculate one student-mentor pair's compatibility score

--- 1947_maximum_compatibility_score_sum/solution.py
+++ 1947_maximum_compatibility_score_sum/solution.py
@@ -2,5 +2,13 @@ class Solution:
     def maxCompatibilitySum(self, students, mentors):
         return 0
 
+    def calculateCompatibilityScore(self, studentAnswers, mentorAnswers):
+        score = 0
+        # Problem constraints guarantees the lists are the same length.
+        for answerIndex in range(len(studentAnswers)):
+            if (studentAnswers[answerIndex] == mentorAnswers[answerIndex]): 
+                score += 1
+        return score
+
     def solve(self, inputs):
         return self.maxCompatibilitySum(inputs['students'], inputs['mentors'])
