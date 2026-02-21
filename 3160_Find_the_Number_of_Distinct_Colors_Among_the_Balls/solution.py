ab98e6dd924a9c7a27403a809b7493ddc6d01d3b
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-20 23:02:52 -0800

    Initial working solution - probably improvable

--- 3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls/solution.py
+++ 3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls/solution.py
@@ -1,6 +1,45 @@
 class Solution:
-    def problem_name(self):
-        return None
+    def queryResults(self, limit, queries):
+        # Simple solution
+
+        ballColors = {}
+        colorUses = {}
+        uniqueColors = 0
+        result = []
+        for query in queries:
+            ball = query[0]
+            color = query[1]
+            if (ball in ballColors):
+                # Ignore if we set the ball to the same color
+                if (ballColors[ball] == color):
+                    result.append(uniqueColors)
+                    continue
+
+                # Lower the count for the old ball color.
+                oldColor = ballColors[ball]
+                colorUses[oldColor] = max(0, colorUses[oldColor] - 1)
+                if (colorUses[oldColor] <= 0):
+                    uniqueColors -= 1
+      
+            # Update the count for the new color
+            if (color in colorUses and colorUses[color] > 0):
+                colorUses[color] += 1
+            else:
+                colorUses[color] = 1
+                uniqueColors += 1
+
+            ballColors[ball] = color
+            result.append(uniqueColors)
+            print(ballColors, query)
+            
+
+        print(result)
+
+        return result
+
+
+
+        
 
     def solve(self, inputs):
-        return self.problem_name()
+        return self.queryResults(inputs['limit'], inputs['queries'])
