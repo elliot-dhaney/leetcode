6218d3342208729f584229d3894eba0ceef28746
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-24 11:02:06 -0800

    Initial 40 setup

---
+++ 40_combination_sum_2/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        return None
+
+    def solve(self, inputs):
+        return self.combinationSum2(inputs['candidates'], inputs['target'])
---
+++ 40_combination_sum_2/tests.py
@@ -0,0 +1,94 @@
+import time
+import random 
+from solution import Solution
+
+class Test:
+    TESTS = [{
+        'TITLE': 'Provided Test Case 1',
+        'INPUTS': {
+            'candidates': [10,1,2,7,6,1,5],
+            'target': 8
+        },
+        'OUTPUT': [
+            [1,1,6],
+            [1,2,5],
+            [1,7],
+            [2,6]
+        ],
+    }, {
+        'TITLE': 'Provided Test Case 2',
+        'INPUTS': {
+            'candidates': [2,5,2,1,2],
+            'target': 5
+        },
+        'OUTPUT': [
+            [1,2,2],
+            [5]
+        ],
+    }, {
+        'TITLE': 'Include value exactly equal to target',
+        'INPUTS': {
+            'candidates': [10,1,2,7,6,1,5,8],
+            'target': 8
+        },
+        'OUTPUT': [
+            [1,1,6],
+            [1,2,5],
+            [1,7],
+            [2,6],
+            [8]
+        ],
+    }, 
+    ]
+
+    def __init__(self):
+        self.solution = Solution()
+        
+        self.numTests = 0
+        self.generateTests()
+
+    def generateTests(self):
+        for i in range(self.numTests):
+            inp = 0
+            out = 0
+
+            self.TESTS.append({
+                'TITLE': f'Generated Test {i}',
+                'INPUTS': { '': inp },
+                'OUTPUT': out
+            })
+
+    def runTests(self):
+        startTime = time.time()
+        testsRun = 0
+        testsFailed = 0
+        for test in self.TESTS:
+            print(f'Running test {test["TITLE"]}')
+
+            inputs = test['INPUTS']
+            expected = test['OUTPUT']
+
+            testsRun += 1
+            try:
+                output = self.solution.solve(inputs)
+            except Exception as e:
+                print(f'Error occurred while solving: {e}')
+                testsFailed += 1
+                continue
+
+            try:
+                assert(output == expected)
+            except AssertionError:
+                print(f'Test Failed with\nINPUT {inputs}\nEXPECTED {expected}\nACTUAL {output}')
+                testsFailed += 1
+
+        endTime = time.time()
+
+        print(f'Tests Run: {testsRun}')
+        print(f'Tests Succeeded: {testsRun - testsFailed}')
+        print(f'Tests Failed: {testsFailed}')
+
+        print(f'Testing took {endTime - startTime} milliseconds')
+
+Tester = Test()
+Tester.runTests()
