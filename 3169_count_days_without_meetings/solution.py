f717a6bbd603e6606194681b4f088bc3ba4b4898
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-26 14:13:27 -0800

    Add solution

--- 3169_count_days_without_meetings/solution.py
+++ 3169_count_days_without_meetings/solution.py
@@ -1,6 +1,19 @@
 class Solution:
     def countDays(self, days, meetings):
-        return None
+        numDays = days
 
+        meetings.sort()
+        lastDay = -1
+        for meeting in meetings:
+            if (meeting[1] < lastDay):
+                continue
+            elif (meeting[0] <= lastDay):
+                numDays -= (meeting[1] - lastDay)
+            else:
+                numDays -= (meeting[1] - meeting[0] + 1)
+            lastDay = meeting[1]
+
+        return numDays
+        
     def solve(self, inputs):
         return self.countDays(inputs['days'], inputs['meetings'])
