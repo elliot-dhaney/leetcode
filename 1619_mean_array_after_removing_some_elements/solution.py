f36d8add508a8fccfde1fcb2a063be1bf3d5b12b
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-02-27 19:12:45 -0800

    Add solution

--- 1619_mean_array_after_removing_some_elements/solution.py
+++ 1619_mean_array_after_removing_some_elements/solution.py
@@ -1,6 +1,9 @@
 class Solution:
     def trimMean(self, arr):
-        return None
+        arr.sort()
+        perc5 = len(arr) // 20
+        
+        return sum(arr[perc5 : 19*perc5]) / (18 * perc5)
 
     def solve(self, inputs):
         return self.trimMean(inputs['arr'])
