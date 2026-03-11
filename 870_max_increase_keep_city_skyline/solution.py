4095c932fa8cc0594bf18e525f0f86df9f0f8ddc
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-03-11 16:37:24 -0700

    Initial 807 setup

---
+++ 870_max_increase_keep_city_skyline/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid):
+        return None
+
+    def solve(self, inputs):
+        return self.maxIncreaseKeepingSkyline(inputs['grid'])
---
+++ 870_max_increase_keep_city_skyline/tests.py
@@ -0,0 +1,71 @@
+import time
+import random 
+from solution import Solution
+
+class Test:
+    TESTS = [{
+        'TITLE': 'Provided Test Case 1',
+        'INPUTS': {
+            'grid': [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]],
+        },
+        'OUTPUT': 35,
+    }, {
+        'TITLE': 'Provided Test Case 2',
+        'INPUTS': {
+            'grid': [[0,0,0],[0,0,0],[0,0,0]],
+        },
+        'OUTPUT': 0,
+    }, 
+    ]
+
+    def __init__(self):
+        self.solution = Solution()
+        
+        self.numTests = 0
+        self.generateTests()
+
+    def generateTests(self):
+        for i in range(self.numTests):
+            inp = 0
+            out = 0
+
+            self.TESTS.append({
+                'TITLE': f'Generated Test {i}',
+                'INPUTS': { '': inp },
+                'OUTPUT': out
+            })
+
+    def runTests(self):
+        startTime = time.time()
+        testsRun = 0
+        testsFailed = 0
+        for test in self.TESTS:
+            print(f'Running test {test["TITLE"]}')
+
+            inputs = test['INPUTS']
+            expected = test['OUTPUT']
+
+            testsRun += 1
+            try:
+                output = self.solution.solve(inputs)
+            except Exception as e:
+                print(f'Error occurred while solving: {e}')
+                testsFailed += 1
+                continue
+
+            try:
+                assert(output == expected)
+            except AssertionError:
+                print(f'Test Failed with\nINPUT {inputs}\nEXPECTED {expected}\nACTUAL {output}')
+                testsFailed += 1
+
+        endTime = time.time()
+
+        print(f'Tests Run: {testsRun}')
+        print(f'Tests Succeeded: {testsRun - testsFailed}')
+        print(f'Tests Failed: {testsFailed}')
+
+        print(f'Testing took {endTime - startTime} milliseconds')
+
+Tester = Test()
+Tester.runTests()
