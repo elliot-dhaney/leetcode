82357eca0be93cbe9b8f569bedf64935fcdbd092
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-03-11 15:33:14 -0700

    Add solution

--- 1720_decode_xored_array/solution.py
+++ 1720_decode_xored_array/solution.py
@@ -1,6 +1,10 @@
 class Solution:
     def decode(self, encoded, first):
-        return None
+        arr = [first]
+        for val in encoded:
+            arr.append(val ^ first)
+            first = val ^ first
+        return arr
 
     def solve(self, inputs):
         return self.decode(inputs['encoded'], inputs['first'])
