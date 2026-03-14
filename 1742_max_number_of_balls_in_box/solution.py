2f235a3be57bfd68d28be8886f85cbc8f9d840e0
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-03-14 15:41:22 -0700

    Initial 1742 setup

---
+++ 1742_max_number_of_balls_in_box/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countBalls(self, lowLimit, highLimit):
+        return None
+
+    def solve(self, inputs):
+        return self.countBalls(inputs['lowLimit'], inputs['highLimit'])
