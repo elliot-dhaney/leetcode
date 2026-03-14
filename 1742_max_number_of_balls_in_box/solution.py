6a3f66d8fd8ba18ec3c8b3e0f7ecf1abe8b10bfe
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-03-14 15:49:53 -0700

    Add solution

--- 1742_max_number_of_balls_in_box/solution.py
+++ 1742_max_number_of_balls_in_box/solution.py
@@ -1,6 +1,26 @@
 class Solution:
     def countBalls(self, lowLimit, highLimit):
-        return None
+        counts = dict()
+        maxCount = 1
+
+        # Count digitSums, and keep track of the max count.
+        for ball in range(lowLimit, highLimit + 1):
+            digitSum = self.sumDigits(ball)
+            if (digitSum in counts):
+                counts[digitSum] += 1
+                if (counts[digitSum] > maxCount):
+                    maxCount = counts[digitSum]
+            else:
+                counts[digitSum] = 1
+
+        return maxCount
+
+    def sumDigits(self, digits):
+        digitSum = 0
+        while (digits > 0):
+            digitSum += (digits % 10)
+            digits = digits // 10
+        return digitSum
 
     def solve(self, inputs):
         return self.countBalls(inputs['lowLimit'], inputs['highLimit'])
