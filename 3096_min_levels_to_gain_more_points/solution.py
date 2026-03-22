85947cca8350ad3cf149231c93c63206911b54ec
ehaney1 <35716995+ehaney1@users.noreply.github.com>
2026-03-21 17:07:51 -0700

    Add solution

--- 3096_min_levels_to_gain_more_points/solution.py
+++ 3096_min_levels_to_gain_more_points/solution.py
@@ -1,6 +1,22 @@
 class Solution:
     def minimumLevels(self, possible):
-        return None
+        # Total available points is constant, so just play games until we reach 
+        # a winning state.
+        numGames = len(possible)
+
+        leftScore = 2 * possible[0] - 1
+        rightScore = (2 * sum(possible) - numGames) - leftScore # total score - first game
+
+        index = 1
+        while (leftScore <= rightScore and index < numGames - 1):
+            scoreFromGame = 2 * possible[index] - 1
+            leftScore += scoreFromGame
+            rightScore -= scoreFromGame
+            index += 1
+        
+        if (leftScore > rightScore):
+            return index
+        return -1
 
     def solve(self, inputs):
         return self.minimumLevels(inputs['possible'])
